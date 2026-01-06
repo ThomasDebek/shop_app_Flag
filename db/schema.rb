a08ffcdcb7937e187f9e643d6efd1abc9eea89bc
ThomasDebek <tom.debek@gmail.com>
2026-01-06 22:29:28 +0100

    create orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_06_212816) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_06_212903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal "total"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_06_212816) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "orders", "users"
 end
